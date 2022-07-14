21799587c8b45641f2b4617a6f6c31927029480b
gursewak22 <63358296+gursewak22@users.noreply.github.com>
2022-07-14 17:06:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+for(var i =0 ; i < document.querySelectorAll(".drum").length ;++i){
+    document.querySelectorAll("button")[i].addEventListener('click',function(){
+        var btninnerhtml = this.innerHTML;
+        makeSound(btninnerhtml);
+        btnanimation(btninnerhtml);
+
+    });
+    }
+    document.addEventListener("keydown" , function(event){
+        makeSound(event.key);
+        btnanimation(event.key);
+    });
+    function makeSound(key){
+        switch (key) {
+            case "w":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+                case "a":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+                case "s":
+                    var audio = new Audio('sounds/snare.mp3');
+                    audio.play();
+                break;
+                case "d":
+                    var audio = new Audio('sounds/tom-1.mp3');
+                    audio.play();
+                break;
+                case "j":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+                case "k":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+                case "l":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;
+             
+                    
+            default:console.log(key);
+                
+        }
+    }
+    function btnanimation(key){
+            document.querySelector("." + key).classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector("." +key ).classList.remove("pressed");
+            },100);
+    }
